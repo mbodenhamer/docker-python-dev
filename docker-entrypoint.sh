4604afbde5e98e29468d9763635352b613b80543
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-04-10 23:02:52 -0500

    Adding re-chown functionality to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exec "$@"
+# Execute whatever was passed into Docker run, but retain control of the shell
+exec "$@" &>/dev/tty | true
+
+# Reset ownership of affected files
+find /app -uid 0 -exec chown -h $BE_UID {} \+
+find /app -gid 0 -exec chgrp -h $BE_GID {} \+
