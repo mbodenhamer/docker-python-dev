3ecc67ed6d2537f43dc8f34d0d9faa07627b14a3
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-05-05 17:37:38 -0500

    Improving docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Execute whatever was passed into Docker run, but retain control of the shell
-exec "$@" &>/dev/tty | true
+# Execute whatever was passed into docker run, but retain control of the shell
+( exec "$@" )
+EXIT=$?
 
 # Reset ownership of affected files
+set -e
 find /app -uid 0 -exec chown -h $BE_UID {} \+
 find /app -gid 0 -exec chgrp -h $BE_GID {} \+
+exit $EXIT
